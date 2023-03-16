9320bc43085a212941d085831857a778c32dd710
ikhumwa edmond mulera <edmondmulera55@gmail.com>
2023-03-16 09:41:56 -0700

    c language

---
+++ c_programing/hello_word.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Aways 0 (success)
+ */
+int main(void)
+{   /* library functions*/
+
+	int a = 3;
+	float b = 3.3;
+	char c = 'c';
+
+	printf("this is an interger %d \n", a);
+	printf("this is a float %f \n", b);
+	printf("this is a character %c \n", c);
+	return (0);
+}
---
+++ c_programing/stracture.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Emtry point
+ *
+ * Return: Aways 0 (success)
+ */
+int main(void)
+{
+	printf("hello world \n");
+	return (0);
+}
